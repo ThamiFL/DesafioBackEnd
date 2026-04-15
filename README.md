11b5b07f24e486d6f941a210c0fd8b81a4733dd1
Thamires Brito <thamifl29@gmail.com>
2026-04-14 21:05:15 -0300

    README e configuração do cors

---
+++ README.md
@@ -0,0 +1,43 @@
+---
+## 📌 Sobre o projeto
+
+Este projeto foi desenvolvido para auxiliar o registro e acompanhamento de pessoas desaparecidas durante situações de enchentes. Esse sistema permite cadastrar indivíduos desaparecidos, armazenar suas informações e atualizar seu status conforme novas informações, facilitando a organização.
+---
+
+---
+
+## 🛠️ Tecnologias utilizadas
+
+Backend
+
+- Node.js
+- Express
+- SQLite
+
+---
+
+Frontend
+
+- React
+- Axios
+
+---
+
+## 📊 Estrutura do banco de dados
+
+Tabela: desaparecidos
+
+|                    |
+| ------------------ |
+| id                 |
+| nome               |
+| idade              |
+| foto               |
+| caracteristicas    |
+| roupa              |
+| ultimo_local       |
+| condicoes_saude    |
+| nome_responsavel   |
+| contato            |
+| status             |
+| ultima_atualizacao |
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { criarBanco } = require("./database");
 
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
+app.use("/assets", express.static("assets"));
 app.use(express.json());
 
 //Rota principal
@@ -32,9 +34,74 @@ app.get("/desaparecidos/:id", async (req, res) => {
   res.json(desaparecidoEspecifico);
 });
 
+//Postando
+
+app.post("/desaparecidos", async (req, res) => {
+  const {
+    nome,
+    idade,
+    foto,
+    caracteristicas,
+    roupa,
+    ultimo_local,
+    condicoes_saude,
+    nome_responsavel,
+    contato_responsavel,
+    endereco,
+  } = req.body;
+  const db = await criarBanco();
+  await db.run(
+    `INSERT INTO desaparecidos(nome, idade, foto, caracteristicas, roupa, ultimo_local, condicoes_saude, nome_responsavel, contato_responsavel, endereco) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+      nome,
+      idade,
+      foto,
+      caracteristicas,
+      roupa,
+      ultimo_local,
+      condicoes_saude,
+      nome_responsavel,
+      contato_responsavel,
+      endereco,
+    ],
+  );
+
+  res.send(`Cadastro feito com sucesso!`);
+});
+
+//Atualizando
+
+app.put("/desaparecidos", async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  const db = await criarBanco();
+  await db.run(
+    `
+  UPDATE desaparecidos
+  SET status = ?
+  WHERE id = ?`,
+    [status, id],
+  );
+  res.send(`Registro atualizado`);
+});
+
+//Removendo
+app.delete("/desaparecidos/:id", async (req, res) => {
+  const { id } = req.params;
+  const db = await criarBanco();
+  await db.run(
+    `
+  DELETE FROM desaparecidos WHERE id = ?
+  `,
+    [id],
+  );
+
+  res.send(`Registro removido com sucesso`);
+});
+
 //Porta
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
